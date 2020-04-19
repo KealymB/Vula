75d456e92b0ebf2d568fc57f4541042949116a59
KealymB <BRMKEA001@myuct.ac.za>
2020-04-19 10:29:38 +0200

    test

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+  ios: `Press Cmd+R to reload,\nCmd+D or shake fordev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
