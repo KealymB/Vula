dbe377942be49a1070fdf56c210a291f625be199
KealymB <BRMKEA001@myuct.ac.za>
2020-04-20 08:20:50 +0200

    need to remove cookie manager

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from 'react-native';
+let CookieManager = require("@react-native-community/cookies");
+var FormData = require('form-data')
+
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake fordev menu`,
@@ -7,6 +11,41 @@ const instructions = Platform.select({
 });
 
 export default function App() {
+  let formdata = new FormData();
+    formdata.append('_username', 'USERNAME')
+    formdata.append('_password', 'PASSWORD')
+
+    CookieManager.clearAll()
+      .then((res) => {
+        console.log('CookieManager.clearAll =>', res);
+      });
+
+    function makeRequest(path, params) {
+      fetch(path, {
+        method: 'POST',
+        headers: {
+          'User-Agent': 'react-native',
+          'Connection': 'keep-alive'
+        },
+        body: params
+      }).then((response) => response.headers['map']['set-cookie']).then(cookie => {
+        AsyncStorage.setItem('cookie', JSON.stringify(cookie))
+      }).catch(err => {
+        console.log(err)
+      })
+
+
+    }
+
+    async function get() {
+      let a = await makeRequest("https://vula.uct.ac.za/direct/session/new", formdata)
+      let cookie = await AsyncStorage.getItem('cookie')
+      alert('Cookie: ' + cookie)
+    }
+
+    get()
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
