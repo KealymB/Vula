ef0ebb91d4c8057a20ad567f6a74904c362a33a8
Sheldon Reay <ryxshe002@myuct.ac.za>
2020-06-05 21:48:13 +0200

    Added DocViewer functionality in new window. Working for iOS

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,83 +1,42 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, ScrollView, } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, ScrollView} from 'react-native';
 import Collapsible from 'react-native-collapsible';
-<<<<<<< HEAD
-import * as FileSystem from 'expo-file-system';
-
-
-async function download(url, name) {
-    console.log(url)
-    try {
-        await FileSystem.makeDirectoryAsync(
-            FileSystem.documentDirectory,
-            {
-                intermediates: true
-            }
-        )
-    } catch (e) {
-        console.log(e)
-    }
-
-    await FileSystem.downloadAsync(
-        url,
-        FileSystem.documentDirectory + name
-    )
-        .then(({ uri }) => {
-            console.log('Finished downloading to:', uri);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-
-    try {
-        console.log(await FileSystem.getInfoAsync(FileSystem.documentDirectory + name))
-    } catch (e) {
-        console.log(e)
-    }
-}
-=======
 import DocViewer from './DocViewer';
->>>>>>> ffdee18d155aeaa2e95308e6e272bd61c95348ea
+import { withNavigation} from '@react-navigation/compat'
 
 class Accordion extends Component {
     state = {
         collapsed: true,
         docView: false,
     };
-
-<<<<<<< HEAD
-    toggleExpanded = (url, type, name) => {
-        if (type == 'collection') {
-            this.setState({ collapsed: !this.state.collapsed });
-        } else {
-            download(url, name);
-=======
+    
     toggleExpanded = (type) => {
         if(type == 'collection'){
             this.setState({ collapsed: !this.state.collapsed });
         }else{
+            console.log('setting docview')
           this.setState({ collapsed: this.state.collapsed, docView: true });
->>>>>>> ffdee18d155aeaa2e95308e6e272bd61c95348ea
+          this.props.navigation.navigate('DocViewer', 
+          {
+              url: this.props.url,
+              title: this.props.title
+          })
         }
     };
-
     render() {
-      switch (this.state.docView){
-        case true:
-          return (
+    //   switch (this.state.docView){
+    //     case true:
+    //       return (
+              
+    //         <View style={{flex:1}}>
+    //             <DocViewer url={this.props.url} name={this.props.title}/>
+    //         </View>
+    //     );
+    //     default:
+          return (            
             <View>
-                <DocViewer url={this.props.url} name={this.props.title}/>
-            </View>
-        );
-        default:
-          return (
-            <View>
-<<<<<<< HEAD
-                <TouchableOpacity onPress={() => { this.toggleExpanded(this.props.url, this.props.type, this.props.title)}}>
-=======
                 <TouchableOpacity onPress={()=>{this.toggleExpanded( this.props.type)}}>
->>>>>>> ffdee18d155aeaa2e95308e6e272bd61c95348ea
                     <Text style={this.state.collapsed ? styles.titleclosed : styles.titleopen}>{this.props.title}</Text>
                     <Collapsible collapsed={this.state.collapsed}>
                         {this.props.content}
@@ -86,18 +45,18 @@ class Accordion extends Component {
             </View>
         );
       }
-    }
+    //}
 }
-export default Accordion;
+export default withNavigation(Accordion);
 
 const styles = StyleSheet.create({
     titleopen: {
-        color: 'white',
+        color:'white',
         fontSize: 30,
     },
     titleclosed: {
-        color: 'black',
-        fontSize: 20,
+        color:'black',
+        fontSize:20,
         paddingLeft: 15
     }
 })
--- components/DocViewer.js
+++ components/DocViewer.js
@@ -1,49 +1,80 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Button} from 'react-native';
-import {WebView} from 'react-native-webview';
+import { View, TouchableOpacity, Text, StyleSheet, Button, SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
 import * as Linking from 'expo-linking';
-
-async function download (url, name) {
-    console.log(url)
-    try {
-        await FileSystem.makeDirectoryAsync(
-          FileSystem.documentDirectory,
-          {
-            intermediates: true
-          }
-        )
-      } catch (e) {
-        console.log(e)
+import { withNavigation, getParam, NavigationActions, StackActions } from '@react-navigation/compat'
+async function download(url, name) {
+  console.log(url)
+  try {
+    await FileSystem.makeDirectoryAsync(
+      FileSystem.documentDirectory,
+      {
+        intermediates: true
       }
+    )
+  } catch (e) {
+    console.log(e)
+  }
 
-      await FileSystem.downloadAsync(
-        url,
-        FileSystem.documentDirectory+name
-      )
-        .then(({ uri }) => {
-          console.log('Finished downloading to:', uri);
-          iconTestPath=FileSystem.documentDirectory+name;
-        })
-        .catch(error => {
-          console.error(error);
-      });
+  await FileSystem.downloadAsync(
+    url,
+    FileSystem.documentDirectory + name
+  )
+    .then(({ uri }) => {
+      console.log('Finished downloading to:', uri);
+      iconTestPath = FileSystem.documentDirectory + name;
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
-      try {
-        console.log(await FileSystem.getInfoAsync(FileSystem.documentDirectory+name))
-      } catch (e) {
-        console.log(e)
-      }
+  try {
+    console.log(await FileSystem.getInfoAsync(FileSystem.documentDirectory + name))
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 class DocViewer extends Component {
-    render() {
-        console.log(this.props.url);
-        return (
-            <View>
-                <WebView source={{ uri: 'https://vula.uct.ac.za/access/content/group/4525ef7b-8431-46b1-bd92-2fb0687c2dcb/Week%202%20-%2026%20July/RD1_Wk2%20Held.D_The%20Great%20Globalization%20Debate.pdf'}}/>
-            </View>
-        );
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: props.route.params.url,
+      title: props.route.params.title,
     }
+    console.log(this.state.url)
+  }
+  render() {
+    const navigation = this.props
+    console.log(this.props.url);
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#1f4166' }}>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 50, backgroundColor: '#1f4166', flexDirection: 'row', alignContent: 'center' }}>
+            <View style={{justifyContent:'flex-start'}}>
+              <TouchableOpacity onPress={() => { this.props.navigation.goBack() }}>
+                <Text style={{ fontSize: 20, color: 'white', paddingLeft: 10, paddingTop: 10, justifyContent: 'center' }}>
+                  Go Back
+                  
+            </Text>
+            </TouchableOpacity>
+            </View>
+              <View style={{paddingLeft: 30, paddingTop:15}}>
+                <Text style={{color:'white', fontSize:15}}>{this.state.title}</Text>
+              </View>
+            
+          </View>
+          <View style={{ backgroundColor: 'yellow', flex: 1 }}>
+            <WebView
+              source={{ uri: this.state.url }}
+              sharedCookiesEnabled={true}
+              thirdPartyCookiesEnabled={true}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
-export default DocViewer;
+export default withNavigation(DocViewer);
