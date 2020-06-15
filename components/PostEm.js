d1129a73c44316bc856e0784d795ae2f229f74ee
Sheldon Reay <ryxshe002@myuct.ac.za>
2020-06-15 22:01:35 +0200

    Injected JS into Webview to remove header

--- components/PostEm.js
+++ components/PostEm.js
@@ -7,15 +7,23 @@ import { WebView } from 'react-native-webview';
 
 const screenWidth = Math.round(Dimensions.get('window').width); //used to set content width
 const screenHeight = Math.round(Dimensions.get('window').height); //used to set content height
-
+const removeHeader = 'const elements = document.getElementsByClassName("Mrphs-header"); while (elements.length > 0) elements[0].remove();'
 //<WebView source={{ uri: this.props.url}} sharedCookiesEnabled={true} thirdPartyCookiesEnabled={true}/>
 
 class PostEm extends Component {
+    
     render() {
         var url = 'https://vula.uct.ac.za/portal/site/'+this.props.currSite.key+'/tool/'+this.props.toolID
         return (
             <View style={styles.container}>
-                <WebView source={{ uri: url}} style={styles.view} sharedCookiesEnabled={true} thirdPartyCookiesEnabled={true}/>
+                <WebView source={{ uri: url}} 
+                style={styles.view} 
+                sharedCookiesEnabled={true} 
+                thirdPartyCookiesEnabled={true}
+                javaScriptEnabled={true}
+                scalesPageToFit={false}
+                injectedJavaScript={'function hideHead(){const elements = document.getElementsByClassName("Mrphs-topHeader"); while (elements.length > 0) elements[0].remove();};hideHead();'} 
+                />
             </View>
         );
     }
@@ -37,7 +45,7 @@ const styles = StyleSheet.create({
     container:
     {
         flex: 1,
-        backgroundColor: '#1f4166'
+        backgroundColor: '#1f4166',
     },
     view: {
         height:screenHeight-120,
