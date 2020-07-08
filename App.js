c862084de60d5abd50ed2114097a37a98a9ae333
Sheldon Reay <ryxshe002@myuct.ac.za>
2020-07-08 19:39:33 +0200

    Fixed issue with inconsistent login

--- App.js
+++ App.js
@@ -5,19 +5,47 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Component } from 'react';
-
+import { View, Text } from 'react-native';
 import Login from './screens/Login';
 import Site from './screens/Site';
 import SideBar from './components/SideBar';
 import Loading from './screens/Loading';
 import DocViewer from './components/DocViewer'
 import siteReducer from './reducers/siteReducer';
-
+import AsyncStorage from '@react-native-community/async-storage';
 const Drawer = createDrawerNavigator();
 const store = createStore(siteReducer);
 
+
+
 class App extends Component{
+
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      loggedIn: false
+    }
+    this.getLoginState()
+    
+  }
+    
+
+  getLoginState = async () => {
+    console.log('val: '+this.state.loggedIn)
+    try {
+      const value = await AsyncStorage.getItem('LoginState')
+      if(value !== null) {
+        if(value=='true')
+        this.setState({loggedIn:true})
+        console.log('val: '+this.state.loggedIn)
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  }
   render(){
+    const loggedIn = this.state.loggedIn
     return (
       <Provider store={store}>
         <NavigationContainer>
